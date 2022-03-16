fa2da460e7775f50a34e02b446cbba464349a9ba
Rogalio mouchel <you@example.com>
2022-03-16 19:20:18 +0100

    first commit

---
+++ routes/user.js
@@ -0,0 +1,97 @@
+// Import du package 'express'
+const express = require("express");
+const router = express.Router();
+
+// packages pour encrypter le mot de passe
+const uid2 = require("uid2");
+const SHA256 = require("crypto-js/sha256");
+const encBase64 = require("crypto-js/enc-base64");
+
+// Import du package cloudinary
+const cloudinary = require("cloudinary").v2;
+
+// Import du model User et Offer
+const User = require("../models/User");
+const Offer = require("../models/Offer");
+
+// déclaration de la route signup
+router.post("/user/signup", async (req, res) => {
+  try {
+    // Recherche dans la BDD. Est-ce qu'un utilisateur possède cet email ?
+    const user = await User.findOne({ email: req.fields.email });
+
+    // Si oui, on renvoie un message et on ne procède pas à l'inscription
+    if (user) {
+      res.status(409).json({ message: "This email already has an account" });
+
+      // sinon, on passe à la suite...
+    } else {
+      // l'utilisateur a-t-il bien envoyé les informations requises ?
+      if (req.fields.email && req.fields.password && req.fields.username) {
+        // Si oui, on peut créer ce nouvel utilisateur
+
+        // Étape 1 : encrypter le mot de passe
+        // Générer le token et encrypter le mot de passe
+        const token = uid2(64);
+        const salt = uid2(64);
+        const hash = SHA256(req.fields.password + salt).toString(encBase64);
+
+        // Étape 2 : créer le nouvel utilisateur
+        const newUser = new User({
+          email: req.fields.email,
+          token: token,
+          hash: hash,
+          salt: salt,
+          account: {
+            username: req.fields.username,
+            phone: req.fields.phone,
+          },
+        });
+
+        // Étape 3 : sauvegarder ce nouvel utilisateur dans la BDD
+        await newUser.save();
+        res.status(200).json({
+          _id: newUser._id,
+          email: newUser.email,
+          token: newUser.token,
+          account: newUser.account,
+        });
+      } else {
+        // l'utilisateur n'a pas envoyé les informations requises ?
+        res.status(400).json({ message: "Missing parameters" });
+      }
+    }
+  } catch (error) {
+    console.log(error.message);
+    res.status(400).json({ message: error.message });
+  }
+});
+
+// déclaration de la route login
+router.post("/user/login", async (req, res) => {
+  try {
+    const user = await User.findOne({ email: req.fields.email });
+
+    if (user) {
+      if (
+        SHA256(req.fields.password + user.salt).toString(encBase64) ===
+        user.hash
+      ) {
+        res.status(200).json({
+          _id: user._id,
+          token: user.token,
+          account: user.account,
+        });
+      } else {
+        res.status(401).json({ error: "Unauthorized" });
+      }
+    } else {
+      res.status(400).json({ message: "User not found" });
+    }
+  } catch (error) {
+    console.log(error.message);
+    res.json({ message: error.message });
+  }
+});
+
+module.exports = router;
